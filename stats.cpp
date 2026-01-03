03294d600ff723abc9ad22312dfafbf65690d706
Joshua E. Roybal <u4ysev@gmail.com>
2026-01-03 13:36:21 -0600

    Add files via upload
    Better Box-Muller truncated normal distribution method.

--- stats.cpp
+++ stats.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <cstdlib>
-#include <cfloat>
 #include "stats.hpp"
+#include "util.hpp"
 
-static const int PI = 3.1415926539;
+static const float PI = 3.1415926539F;
 
 float dataSet::random_() const
 {
-    return rand() / (float(RAND_MAX) + 2e-36);
+    float z = rand() / float(RAND_MAX);
+    while (z == 0)
+        z = rand() / float(RAND_MAX);
+    return z;
 }
 
 void dataSet::set_uniform_(int n)
@@ -32,24 +35,22 @@ void dataSet::set_normal_(int n, float mu, float sigma)
 
 void dataSet::set_scaled_normal_(int n)
 {
-    float mu = 0.0;
-    float sigma = 0.997 / 3.0;
+    float sigma = 0.9973F / 3.0F;
     x_.clear();
     x_.resize(n);
-    for (std::vector<float>::iterator it = x_.begin(); it != x_.end(); ++it) {
-        if ((it - x_.begin()) % 2) {
-            *it = mu + (sigma * sqrt(-2*log(random_())) * cos(2*PI*random_()));
-            while (*it <= -1.0 || *it >= 1.0)
-                *it = mu + (sigma * sqrt(-2*log(random_())) * cos(2*PI*random_()));
-
+    for (int i = 0; i < n; i += 2)
+        while (true) {
+            float u = random_();
+            float v = random_();
+            float r = sqrt(-2.0F * log(u));
+            float z1 = sigma * r * cos(2.0F * PI * v);
+            float z2 = sigma * r * sin(2.0F * PI * v);
+            if (z1 >= -1.0F && z1 <= 1.0F && z2 >= -1.0F && z2 <= 1.0F) {
+                x_[i] = z1;
+                if ((i + 1) < n) x_[i + 1] = z2;
+                break;
+            }
         }
-        else {
-            *it = mu + (sigma * sqrt(-2*log(random_())) * sin(2*PI*random_()));
-            while (*it <= -1.0 || *it >= 1.0)
-                *it = mu + (sigma * sqrt(-2*log(random_())) * cos(2*PI*random_()));
-
-        }
-    }
 }
 
 void dataSet::print_dataSet_() const
